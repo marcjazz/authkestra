b050b5f5d1db86690e4720dca4ee93deb47545e6
marcjazz <marcjazz@users.noreply.github.com>
2026-02-03 23:26:52 +0000

    deploy: 16781fe1484ccc4523444362c0fb3f8d7e5864e3

--- trait.impl/authkestra_core/trait.CredentialsProvider.js
+++ trait.impl/authkestra_core/trait.CredentialsProvider.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["actix_credentials",[["impl CredentialsProvider for <a class=\"struct\" href=\"actix_credentials/struct.MyCredentialsProvider.html\" title=\"struct actix_credentials::MyCredentialsProvider\">MyCredentialsProvider</a>"]]],["axum_credentials",[["impl CredentialsProvider for <a class=\"struct\" href=\"axum_credentials/struct.MyCredentialsProvider.html\" title=\"struct axum_credentials::MyCredentialsProvider\">MyCredentialsProvider</a>"]]]]);
+    var implementors = Object.fromEntries([["actix_credentials",[["impl <a class=\"trait\" href=\"authkestra_core/trait.CredentialsProvider.html\" title=\"trait authkestra_core::CredentialsProvider\">CredentialsProvider</a> for <a class=\"struct\" href=\"actix_credentials/struct.MyCredentialsProvider.html\" title=\"struct actix_credentials::MyCredentialsProvider\">MyCredentialsProvider</a>"]]],["axum_credentials",[["impl <a class=\"trait\" href=\"authkestra_core/trait.CredentialsProvider.html\" title=\"trait authkestra_core::CredentialsProvider\">CredentialsProvider</a> for <a class=\"struct\" href=\"axum_credentials/struct.MyCredentialsProvider.html\" title=\"struct axum_credentials::MyCredentialsProvider\">MyCredentialsProvider</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[221,219]}
+//{"start":57,"fragment_lengths":[353,351]}
--- trait.impl/authkestra_core/trait.UserMapper.js
+++ trait.impl/authkestra_core/trait.UserMapper.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["actix_credentials",[["impl UserMapper for <a class=\"struct\" href=\"actix_credentials/struct.MyUserMapper.html\" title=\"struct actix_credentials::MyUserMapper\">MyUserMapper</a>"]]],["authkestra_core",[]],["axum_credentials",[["impl UserMapper for <a class=\"struct\" href=\"axum_credentials/struct.SqlxUserMapper.html\" title=\"struct axum_credentials::SqlxUserMapper\">SqlxUserMapper</a>"]]]]);
+    var implementors = Object.fromEntries([["actix_credentials",[["impl <a class=\"trait\" href=\"authkestra_core/trait.UserMapper.html\" title=\"trait authkestra_core::UserMapper\">UserMapper</a> for <a class=\"struct\" href=\"actix_credentials/struct.MyUserMapper.html\" title=\"struct actix_credentials::MyUserMapper\">MyUserMapper</a>"]]],["authkestra_core",[]],["axum_credentials",[["impl <a class=\"trait\" href=\"authkestra_core/trait.UserMapper.html\" title=\"trait authkestra_core::UserMapper\">UserMapper</a> for <a class=\"struct\" href=\"axum_credentials/struct.SqlxUserMapper.html\" title=\"struct axum_credentials::SqlxUserMapper\">SqlxUserMapper</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[185,23,189]}
+//{"start":57,"fragment_lengths":[299,23,303]}
