e80d6b70b3343c87d47b40f450c7009894a05414
marcjazz <marcjazz@users.noreply.github.com>
2026-05-25 09:52:49 +0000

    deploy: ab74fddb424a2e36ddbfcc49c04ccdefb403d90c

--- trait.impl/core/cmp/trait.Eq.js
+++ trait.impl/core/cmp/trait.Eq.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["authkestra_engine",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"authkestra_engine/auth/enum.SameSite.html\" title=\"enum authkestra_engine::auth::SameSite\">SameSite</a>",0]]],["authkestra_guard",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"authkestra_guard/enum.AuthPolicy.html\" title=\"enum authkestra_guard::AuthPolicy\">AuthPolicy</a>",0]]]]);
+    const implementors = Object.fromEntries([["authkestra_engine",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"authkestra_engine/auth/enum.SameSite.html\" title=\"enum authkestra_engine::auth::SameSite\">SameSite</a>",0]]],["authkestra_resource",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"authkestra_resource/enum.AuthPolicy.html\" title=\"enum authkestra_resource::AuthPolicy\">AuthPolicy</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[290,283]}
+//{"start":59,"fragment_lengths":[290,292]}
--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["authkestra_engine",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"authkestra_engine/auth/enum.SameSite.html\" title=\"enum authkestra_engine::auth::SameSite\">SameSite</a>",0]]],["authkestra_guard",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"authkestra_guard/enum.AuthPolicy.html\" title=\"enum authkestra_guard::AuthPolicy\">AuthPolicy</a>",0]]]]);
+    const implementors = Object.fromEntries([["authkestra_engine",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"authkestra_engine/auth/enum.SameSite.html\" title=\"enum authkestra_engine::auth::SameSite\">SameSite</a>",0]]],["authkestra_resource",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"authkestra_resource/enum.AuthPolicy.html\" title=\"enum authkestra_resource::AuthPolicy\">AuthPolicy</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[302,295]}
+//{"start":59,"fragment_lengths":[302,304]}
