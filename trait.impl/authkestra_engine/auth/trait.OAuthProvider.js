07f9f5df58183911f47069f8387e9a03a68ffde5
marcjazz <marcjazz@users.noreply.github.com>
2026-07-23 10:50:41 +0000

    deploy: e839ac49a4d9cb6b63e69045f1deaf6bb5daff66

--- trait.impl/authkestra_engine/auth/trait.OAuthProvider.js
+++ trait.impl/authkestra_engine/auth/trait.OAuthProvider.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["authkestra_oidc",[["impl OAuthProvider for <a class=\"struct\" href=\"authkestra_oidc/provider/struct.OidcProvider.html\" title=\"struct authkestra_oidc::provider::OidcProvider\">OidcProvider</a>",0]]]]);
+    const implementors = Object.fromEntries([["authkestra_oidc",[["impl <a class=\"trait\" href=\"authkestra_engine/auth/trait.OAuthProvider.html\" title=\"trait authkestra_engine::auth::OAuthProvider\">OAuthProvider</a> for <a class=\"struct\" href=\"authkestra_oidc/provider/struct.OidcProvider.html\" title=\"struct authkestra_oidc::provider::OidcProvider\">OidcProvider</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[203]}
+//{"start":59,"fragment_lengths":[338]}
