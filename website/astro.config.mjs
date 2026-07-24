4a5c6bf43bd6fae89e2c90d2b9364bdf79665172
Marco Kuidja <kuidjamarco@gmail.com>
2026-07-24 22:32:44 +0200

    chore(website): update base url for github pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,7 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://authkestra.com',
+	site: 'https://marcjazz.github.io',
+	base: '/authkestra',
 
 	integrations: [
 		starlight({
